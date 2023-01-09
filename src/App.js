04c76d3333d40455ab711509a2fe5664eeae1855
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2023-01-09 13:04:59 +0100

    navbar routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
-import { LandingPage } from "./component/pages";
+import { Route, Routes, BrowserRouter} from "react-router-dom"
+import { LandingPage, LiveGames, News, Play, Puzzles } from "./component/pages";
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path ='/' element ={<LandingPage />}/>
+        <Route exact path ='/livegames' element ={<LiveGames />}/>
+        <Route exact path ='/news' element ={<News />}/>
+        <Route exact path ='/play' element ={<Play />}/>
+        <Route exact path ='/puzzles' element ={<Puzzles />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/component/pages/index.js
+++ src/component/pages/index.js
@@ -1,1 +1,5 @@
 export * from "./LandingPage";
+export * from "./LiveGames";
+export * from "./News";
+export * from "./Play";
+export * from "./Puzzles";
